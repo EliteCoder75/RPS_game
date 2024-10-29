c0d250fbbdc2a92149083f2f14915cb104ec228c
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2024-10-29 18:46:15 +0100

    implementation of the getComputerChoice() to return a random computer choice for the game

--- myscript.js
+++ myscript.js
@@ -1,12 +1,58 @@
 
-console.log("hello wold");
+//console.log("hello wold");
 
 
 // getComputerChoice -> using math.random function, we generate a number (0-1) with three probabilities, using simple if link result (rock, paper, scissors) 
 
+
+function getComputerChoice(){
+    let ch = Math.floor(Math.random() * 3);
+
+    if (ch == 0) {
+        return "rock"
+    }
+    else if (ch == 1) {
+        return "paper"
+    }
+    else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice());
+
+
+
+
+
+
+
+
 // human_choice -> given a user, having a prompt, take his input and store it 
+
+/*function getHumanChoice(){
+
+
+}
+*/
 
 // keep track of user and computer scores in vars humanScore, computerScore initialzed to 0
 
 
 
+/*function playRound(humanChoice, computerChoice) {
+    // your code here!
+  }
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  playRound(humanSelection, computerSelection);*/
+
+
+
+
+
+
+
+  
