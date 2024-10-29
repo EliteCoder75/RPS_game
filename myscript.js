03671b5dd61f5291b7adecc9f57d73af1c19b49f
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2024-10-29 18:29:27 +0100

    setup the project and the pseudo code  of it

---
+++ myscript.js
@@ -0,0 +1,12 @@
+
+console.log("hello wold");
+
+
+// getComputerChoice -> using math.random function, we generate a number (0-1) with three probabilities, using simple if link result (rock, paper, scissors) 
+
+// human_choice -> given a user, having a prompt, take his input and store it 
+
+// keep track of user and computer scores in vars humanScore, computerScore initialzed to 0
+
+
+
